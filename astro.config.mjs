9c246ea186a2f7a57567ee7d899460c17cfaaa4a
iAMmrMACKENCHI <66863529+iAMmrMACKENCHI@users.noreply.github.com>
2026-06-16 01:00:41 +0000

    Add base path for GitHub Pages repository deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iAMmrMACKENCHI.github.io/SIE-System-Intelligence-Engineers-/',
+  base: '/SIE-System-Intelligence-Engineers-/',
   output: 'static',
 });
